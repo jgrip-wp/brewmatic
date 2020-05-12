6434bc7b290085bf4bcc1880fdca467955eb496e
yameteh_meepo <yameteh_meepo@163.com>
2020-05-12 09:58:59 +0800

    common

---
+++ temp.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale = 1.0">
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="css/common.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/animate.css"/>
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+  <div id="wrapper">
+
+    <header>
+      <div id="head">
+        <div class="head_inner">
+          <div class="head_inner_box">
+            <h1 class="head_logo"><a href="/"><img src="/images/common/logo_head_pc.png" alt="BREW MATIC"></a></h1>
+            <div class="head_navi">
+              <p class="head_navi_up"><a href="#">About us</a><a href="#">Sustainability</a><a href="#">採用情報</a><a href="#">よくあるご質問</a><span class="head_search"><input type="text"  placeholder="検索する"></span></p>
+              <p class="head_navi_down"><a href="#">ブランド一覧</a><a href="#">サポートのご案内</a><a href="#">コーヒーマシンの選び方</a><a href="#">Life with Coffee</a><a href="#">オンラインショップ</a></p>
+            </div>
+            <div class="head_menu visiblets">
+              <p class="js-navbar"><img src="/images/common/open_menu.png" alt=""></p>
+              <div id="menu" class="visiblets">
+                <div class="menu_serch clearfix">
+                  <p class="menu_serch_inner"><input type="text" placeholder="検索する"></p>
+                  <p class="js-navbar-close"><img src="/images/common/close_menu.png" alt=""></p>
+                </div>
+                <div class="menu_content">
+                  <ul class="menu_link_up">
+                    <li><a href="#">ブランド一覧</a></li>
+                    <li><a href="#">サポートのご案内</a></li>
+                    <li><a href="#">コーヒーマシンの選び方</a></li>
+                    <li><a href="#">Life with Coffee</a></li>
+                    <li><a href="#">オンラインショップ</a></li>
+                  </ul>
+                  <ul class="menu_link_down">
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Sustainability</a></li>
+                    <li><a href="#">採用情報</a></li>
+                    <li><a href="#">よくあるご質問</a></li>
+                  </ul>
+                  <ul class="menu_btn">
+                    <li><a href="#" class="menu_btn_contact">お問合せ</a></li>
+                    <li><a href="#" class="menu_btn_cata">カタログのご請求</a></li>
+                    <li><a href="#" class="menu_btn_show">ショールームのご案内</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /head -->
+    </header>
+
+    <article>
+      <div id="main-contents">
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      </div>
+      <!-- /main-contents --> 
+
+    </article>
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'); ?>
+
+  </div>
+  <!-- /wrapper -->
+</body>
+</html>
