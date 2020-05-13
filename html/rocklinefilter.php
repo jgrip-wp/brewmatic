a25d71eaf28e7b5cbbfdf9a5be37c93093f9e57d
yameteh_meepo <yameteh_meepo@163.com>
2020-05-13 17:57:50 +0800

    rocklinefilter.php

---
+++ html/rocklinefilter.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="/css/common.css" media="all" />
+<link rel="stylesheet" type="text/css" href="/css/animate.css"/>
+<link rel="stylesheet" type="text/css" href="/css/rocklinefilter.css"/>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
+
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/common.js"></script>
+<script type="text/javascript" src="/js/wow.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+  <div id="wrapper">
+    <input type="hidden" id="pageID" value="">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/head.php'); ?>
+
+
+    <article>
+      <div id="contents">
+        <p id="breadcrumb" class="op"><a href="/" class="home fonten">Top</a> > <a href="/brand.php">ブランド一覧</a> > フィルターペーパー</p>
+        <div class="contents_main">
+          <div class="inner">
+            <h2 class="page_title wow fadeIn"><span class="page_title_en fonten">Brand lineup</span>ROCKLINE</h2>
+            <p class="page_title_detail wow fadeIn">1976年の創業以来、世界最高品質を目指すロックライン。<br>コーヒーフィルターペーパーのシェアは北米No.1。</p>
+            <div class="contents_main_box">
+              <div class="rocklinefilter_content">
+                <div class="rocklinefilter_box wow fadeIn">
+                  <div class="rocklinefilter_box_img"><img src="/images/rocklinefilter/img_rocklinefilter01.png" alt="写真"></div>
+                  <div class="rocklinefilter_box_content">
+                    <h4 class="rocklinefilter_box_title">フィルターペーパー</h4>
+                    <p class="rocklinefilter_box_txt">北米で最大シェアを誇るロックライン社製。<br>安定した透過性と丈夫なコーヒーフィルターです。</p>
+                  </div>
+                </div>
+                <!-- /rocklinefilter_box -->
+                <h3 class="page_title_mid wow fadeIn">製品の特徴</h3>
+                <div class="box_yellow wow fadeIn">
+                  <ul class="rocklinefilter_list">
+                    <li><span class="fonten">・</span> 長年の研究結果からおいしいコーヒー抽出に必要な安定した透過性と丈夫さを持ち合わせたフィルターペーパー</li>
+                    <li><span class="fonten">・</span> フィルターは安全性が認められている酸素漂白方式</li>
+                    <li><span class="fonten">・</span> FDA（アメリカ食品医薬局）の規定にもとづいて生産</li>
+                    <li><span class="fonten">・</span> サイズを豊富に取り揃えており、コーヒーチェーンはもちろん、ファミリーレストラン、ファーストフード、コンビニエンスストア、スーパーなど、さまざまな場面で利用可能</li>
+                    <li><span class="fonten">・</span> OCS用の特別サイズやパッケージ梱包も用意</li>
+                  </ul>
+                </div>
+                <!-- /box_yellow -->
+                <h3 class="page_title_mid wow fadeIn">製品スペック</h3>
+                <div class="product_spec wow fadeIn">
+                  <table>
+                    <tr>
+                      <th>フィルター 250</th>
+                      <td>φ25cm</td>
+                      <td>1,000枚/箱</td>
+                      <td>BREAMATIC：EZ Brewer、EZ Thermo Brewer</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 260</th>
+                      <td>φ26cm</td>
+                      <td>1,000枚/箱</td>
+                      <td>BREAMATIC：EZ Brewer、EZ Thermo Brewer</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 270</th>
+                      <td>φ27cm</td>
+                      <td>1,000枚/箱</td>
+                      <td>BREAMATIC：C-22、C-22 Thermo Brewer、B3 Decanter、B3 Thermo Brewer<br>Curtis：ALPHA</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 285</th>
+                      <td>φ28.5cm</td>
+                      <td>500枚/箱</td>
+                      <td>BREAMATIC：C-22、C-22 Thermo Brewer</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 330</th>
+                      <td>φ33cm</td>
+                      <td>500枚/箱</td>
+                      <td>BREAMATIC：C-22、C-22 Thermo Brewer<br>Curtis: G4 Gemini IF、G4 Thermo Pro 1G</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 380</th>
+                      <td>φ38cm</td>
+                      <td>500枚/箱</td>
+                      <td>BREAMATIC：C-22 Thermo Brewer（7.6リットル抽出用）</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 460</th>
+                      <td>φ46cm</td>
+                      <td>500枚/箱</td>
+                      <td>BREAMATIC：Coffee Urn（3ガロンアーン）</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 580</th>
+                      <td>φ58cm</td>
+                      <td>500枚/箱</td>
+                      <td>BREAMATIC：Coffee Urn（6ガロンアーン、10ガロンアーン）</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター 740</th>
+                      <td>φ74cm</td>
+                      <td>250枚/箱</td>
+                      <td>BREAMATIC：Coffee Urn（10ガロンアーン）</td>
+                    </tr>
+                    <tr>
+                      <th>フィルター OCS20P</th>
+                      <td>φ25cm</td>
+                      <td>2,400枚/箱</td>
+                      <td>BREAMATIC：EZ Brewer、EZ Thermo Brewer</td>
+                    </tr>
+                    <tr>
+                      <th>コーンフィルター #4</th>
+                      <td>φ11cm</td>
+                      <td>2,400枚/箱</td>
+                      <td>Curtis：Seraphim、CGC</td>
+                    </tr>
+                  </table>
+                </div>
+                <!-- /product_spec -->
+              </div>
+              <!-- /rocklinefilter_content -->
+            </div>
+          </div>
+        </div>
+        <!-- /contents_main --> 
+      </div>
+      <!-- /contents --> 
+
+    </article>
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'); ?>
+
+  </div>
+  <!-- /wrapper -->
+</body>
+</html>
