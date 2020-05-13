f9bf4324e617a0ee899c5c2aa35813ba3076296e
yameteh_meepo <yameteh_meepo@163.com>
2020-05-13 16:30:18 +0800

    rockline.php

---
+++ html/rockline.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="/css/common.css" media="all" />
+<link rel="stylesheet" type="text/css" href="/css/animate.css"/>
+<link rel="stylesheet" type="text/css" href="/css/rockline.css"/>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
+
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/common.js"></script>
+<script type="text/javascript" src="/js/wow.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+  <div id="wrapper">
+    <input type="hidden" id="pageID" value="">
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/head.php'); ?>
+
+
+    <article>
+      <div id="contents">
+        <p id="breadcrumb" class="op"><a href="/" class="home fonten">Top</a> > <a href="/brand.php">ブランド一覧</a> > ROCKLINE</p>
+        <div class="contents_main">
+          <div class="inner">
+            <h2 class="page_title wow fadeIn"><span class="page_title_en fonten">Brand lineup</span>ROCKLINE</h2>
+            <p class="page_title_detail wow fadeIn">1976年の創業以来、世界最高品質を目指すロックライン。<br>コーヒーフィルターペーパーのシェアは北米No.1。</p>
+            <div class="contents_main_box">
+              <div class="rockline_content">
+                <div class="rockline_banner wow fadeIn"><img src="/images/rockline/img_rockline01.jpg" alt="写真"></div>
+                <p class="rockline_comment op wow fadeIn">コーヒーの個性を最大限に引き出すには、高品質なコーヒーフィルターが不可欠です。<br>ロックラインのコーヒーフィルターは、安定した品質に定評があり世界中のレストラン、カフェ、ホテルなどに選ばれ続けています。</p>
+                <div class="statement_box">
+                  <h3 class="page_title_mid wow fadeIn">Value Statement”PRTE”</h3>
+                  <div class="statement_box_inner wow fadeIn">
+                    <table>
+                      <tr>
+                        <th>Renew</th>
+                        <td>収益は全て未来の事業に再投資。優秀な人材の採用、革新的な技術開発につなげています。</td>
+                      </tr>
+                      <tr>
+                        <th>Respect</th>
+                        <td>従業員、サプライヤー、お客様、コミュニティ、環境を尊重した事業活動を行います。</td>
+                      </tr>
+                      <tr>
+                        <th>Integrity</th>
+                        <td>常に正しい行動を選択します。</td>
+                      </tr>
+                      <tr>
+                        <th>Teamwork</th>
+                        <td>サプライヤーを含めたチームワークを第一に掲げています。</td>
+                      </tr>
+                      <tr>
+                        <th>Excellence</th>
+                        <td>最高品質の製品、プロセス、サービスを提供します。</td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+                <!-- /statement_box -->
+                <div class="rockline_product_list">
+                  <h3 class="page_title_mid wow fadeIn">製品一覧</h3>
+                  <div class="rockline_product_wrap clearfix wow fadeIn">
+                    <div class="rockline_product_item">
+                      <div class="rockline_product_img"><img src="/images/rockline/img_rockline_product01.png" alt="写真"></div>
+                      <p class="rockline_product_name">フィルタ―ペーパー</p>
+                      <p class="rockline_product_txt hlg01">安定した透過性と丈夫な品質を備えたコーヒーフィルター。<br>各種サイズを展開。</p>
+                      <p class="common_btn"><a href="#">詳しく見る</a></p>
+                    </div>
+                  </div>
+                </div>
+                <!-- /rockline_product_list -->
+              </div>
+              <!-- /rockline_content -->
+            </div>
+          </div>
+        </div>
+        <!-- /contents_main --> 
+      </div>
+      <!-- /contents --> 
+
+    </article>
+
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'); ?>
+
+  </div>
+  <!-- /wrapper -->
+</body>
+</html>
