e598ab3f4a3f745019f065618191e05b01fdb335
yameteh_meepo <yameteh_meepo@163.com>
2020-05-14 14:20:39 +0800

    slick

--- html/js/top.js
+++ html/js/top.js
@@ -3,23 +3,21 @@ $(function () {
   $("#wrapper").hide();
   $(document).ready(function () {
     $("#top_logo").delay(1400).fadeOut(800, "", function () {
+      setTimeout(function(){
+        $('.top_main_box').slick({
+          autoplay: true,
+          fade: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          pauseOnHover: false,
+          speed: 3000
+        });
+      },410)
       $("#wrapper").delay(400).fadeIn(800, "", function () {
         new WOW().init();
       });
     });
 
   });
-});
-
-$(function () {
-  $('.top_main_box').slick({
-    autoplay: true,
-    fade: true,
-    arrows: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    pauseOnHover: false,
-    speed: 3000
-  });
-
 });
