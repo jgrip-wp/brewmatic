579b9b519f0cb3786b5cffe6f10c70afba29b1bc
tuosi <ts-web@bpodts.com.cn>
2020-05-14 14:50:28 +0800

    top js

--- html/index.php
+++ html/index.php
@@ -10,13 +10,16 @@
 
 <link rel="stylesheet" type="text/css" href="/css/common.css" media="all" />
 <link rel="stylesheet" type="text/css" href="/css/animate.css"/>
+<link rel="stylesheet" type="text/css" href="/css/slick.css"/>
 <link rel="stylesheet" type="text/css" href="/css/top.css"/>
+
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
 
 <script type="text/javascript" src="/js/jquery.min.js"></script>
 <script type="text/javascript" src="/js/common.js"></script>
 <script type="text/javascript" src="/js/wow.min.js"></script>
+<script type="text/javascript" src="/js/slick.js"></script>
 <script type="text/javascript" src="/js/top.js"></script>
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -29,12 +32,28 @@
 </head>
 <body>
   <div id="wrapper">
-    <input type="hidden" id="pageID" value="">
     <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/head.php'); ?>
 
 
     <article>
       <div id="contents">
+       
+       <div class="top_main_img">
+         <div class="top_main_box">
+           <div>
+             <img src="images/top/main_top01.jpg" alt="">
+           </div>
+           <div>
+             <img src="images/top/main_top01.jpg" alt="">
+           </div>
+           <div>
+             <img src="images/top/main_top01.jpg" alt="">
+           </div>
+         </div>
+       </div>
+       <!-- /top_main_img -->
+       
+       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
        
       </div>
       <!-- /contents -->
--- html/js/top.js
+++ html/js/top.js
@@ -1,14 +1,25 @@
+$(function () {
+  $("body").fadeTo(800, 1);
+  $("#wrapper").hide();
+  $(document).ready(function () {
+    $("#top_logo").delay(1400).fadeOut(800, "", function () {
+      $("#wrapper").delay(400).fadeIn(800, "", function () {
+        new WOW().init();
+      });
+    });
 
-$(function(){
-	$("body").fadeTo(800,1);
-    $("#wrapper").hide();
-    $(document).ready(function () {
-		$("#top_logo").delay(1400).fadeOut(800, "", function(){
-			$("#wrapper").delay(400).fadeIn(800 , "", function(){
-				new WOW().init();
-			});
-		});
-
-	});
+  });
 });
-	
+
+$(function () {
+  $('.top_main_box').slick({
+    autoplay: true,
+    fade: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    speed: 3000
+  });
+
+});
