013ed45fb4c58278208f899db3670c1fed9b43f9
yameteh_meepo <yameteh_meepo@163.com>
2020-05-18 17:28:35 +0800

    baratza init

---
+++ baratza_html/include/head.php
@@ -0,0 +1,43 @@
+    <header>
+      <div id="head">
+        <div class="head_inner">
+          <div class="head_inner_box">
+            <h1 class="head_logo"><a href="/"><img src="/images/common/logo_head_pc.png" alt="BREW MATIC"></a></h1>
+            <div class="head_navi">
+              <p class="head_navi_up"><a href="#" id="gNav01">About us</a><a href="#" id="gNav02">Sustainability</a><a href="#" id="gNav03">採用情報</a><a href="#" id="gNav04">よくあるご質問</a><span class="head_search"><input type="text"  placeholder="検索する"></span></p>
+              <p class="head_navi_down"><a href="#" id="gNav05">ブランド一覧</a><a href="#" id="gNav06">サポートのご案内</a><a href="#" id="gNav07">コーヒーマシンの選び方</a><a href="#" id="gNav08">Life with Coffee</a><a href="#" id="gNav09">オンラインショップ</a></p>
+            </div>
+            <div class="head_menu visiblets">
+              <p class="js-navbar"><img src="/images/common/open_menu.png" alt=""></p>
+              <div id="menu" class="visiblets">
+                <div class="menu_serch clearfix">
+                  <p class="menu_serch_inner"><input type="text" placeholder="検索する"></p>
+                  <p class="js-navbar-close"><img src="/images/common/close_menu.png" alt=""></p>
+                </div>
+                <div class="menu_content">
+                  <ul class="menu_link_up">
+                    <li><a href="#">ブランド一覧</a></li>
+                    <li><a href="#">サポートのご案内</a></li>
+                    <li><a href="#">コーヒーマシンの選び方</a></li>
+                    <li><a href="#">Life with Coffee</a></li>
+                    <li><a href="#">オンラインショップ</a></li>
+                  </ul>
+                  <ul class="menu_link_down">
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Sustainability</a></li>
+                    <li><a href="#">採用情報</a></li>
+                    <li><a href="#">よくあるご質問</a></li>
+                  </ul>
+                  <ul class="menu_btn">
+                    <li><a href="#" class="menu_btn_contact">お問合せ</a></li>
+                    <li><a href="#" class="menu_btn_cata">カタログのご請求</a></li>
+                    <li><a href="#" class="menu_btn_show">ショールームのご案内</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /head -->
+    </header>
---
+++ baratza_html/js/top.js
@@ -0,0 +1,40 @@
+$(function () {
+  $("body").fadeTo(800, 1);
+  $("#wrapper").hide();
+  $(document).ready(function () {
+    $("#top_logo").delay(1400).fadeOut(800, "", function () {
+      setTimeout(function(){
+        $('.top_main_box').slick({
+          autoplay: true,
+          fade: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          pauseOnHover: false,
+          speed: 3000
+        });
+        $('.top_life_slider').slick({
+          autoplay: true,
+          fade: false,
+          arrows: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          pauseOnHover: false,
+          speed: 3000,
+            responsive: [{
+              breakpoint:1080,
+              settings:{
+                slidesToShow: 1,
+              }
+            }]
+        });
+      },410)
+      $("#wrapper").delay(400).fadeIn(800, "", function () {
+        new WOW().init();
+      });
+    });
+
+  });
+});
+
+
