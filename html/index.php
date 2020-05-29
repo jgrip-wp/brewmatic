c1e3ef7dcd1fc1cb34a081f820df36ce6fa7f04c
tuosi <ts-web@bpodts.com.cn>
2020-05-29 18:08:10 +0800

    052906

--- html/index.php
+++ html/index.php
@@ -168,7 +168,7 @@
          <div class="inner">
            <div class="clearfix">
              <div class="top_facebook wow fadeIn">
-               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBrewmatic.japan&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="360" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBrewmatic.japan&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="90%" height="360" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
              </div>
              <div class="ftbox wow fadeIn">
                <p class="top_news_tit fonten">What’s New</p>
---
+++ html/la-rhea-grande2-premium_trader.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="./css/common.css" media="all" />
+<link rel="stylesheet" type="text/css" href="./css/animate.css"/>
+
+<link rel="stylesheet" type="text/css" href="./css/rheavendors.css"/>
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
+
+<script type="text/javascript" src="./js/jquery.min.js"></script>
+<script type="text/javascript" src="./js/common.js"></script>
+<script type="text/javascript" src="./js/wow.min.js"></script>
+<script type="text/javascript" src="./js/jquery.matchHeight-min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+  <div id="wrapper">
+    <input type="hidden" id="pageID" value="gNav09">
+    <?php include './include/head.php'; ?>
+
+
+    <article>
+      <div id="contents">
+        <p id="breadcrumb" class="op"><a href="./" class="home fonten">Top</a> > <a href="#">オンラインショップ</a> > <a href="#" class="home fonten">rheavendors</a> > <span class="home fonten">la-rhea-grande2-premium</span></p>
+        <div class="contents_main js-showhead">
+          <div class="inner">
+            <h2 class="page_title"><span class="page_title_en fonten">Online shop</span>オンラインショップ</h2>
+            <p class="page_title_detail">ブルーマチックジャパンの取り扱いマシン、メンテナンス商品を<br class="visiblepc">お求めいただけるオンラインショップです。</p>
+            <div class="contents_main_box rheaven_main">
+              <ul class="common_logo">
+                <li><a href="#"><img src="./images/common/logo01.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo02.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo03.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo04.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo05.png" alt=""></a></li>
+                <li class="current"><a href="#"><img src="./images/common/logo06.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo07.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo08.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo09.png" alt=""></a></li>
+                <li><a href="#"><img src="./images/common/logo10.png" alt=""></a></li>
+              </ul>
+              <!-- /common_logo -->
+              <div class="rheaven_group">
+                <h3 class="rheaven_group_tit"><img src="./images/rheavendors/logo_rheaven.png" alt="rheavendors"></h3>
+                <div class="rheaven_det_box">
+                  <div class="rheaven_det_in">
+                    <p class="rheaven_det_img"><img src="./images/rheavendors/img_rheaven_det.png" alt="la Rhea GRANDE2 Premium"></p>
+                  </div>
+                  <div class="rheaven_det_in">
+                    <p class="rheaven_det_intro">全自動コーヒーマシン</p>
+                    <h4 class="rheaven_det_tit"><span class="fonten">la Rhea GRANDE2 Premium</span><br>ラ レア グランデ2 プレミアム</h4>
+                    <p class="trader_box">参考上代：<span class="trader_box_remove"><span class="trader_character">￥</span><b class="fonten">1,430,000</b>(税込み)</span></p>
+                    <p class="rheaven_det_price">￥<span class="fonten">0</span><span class="rheaven_det_tax trader_note"> ※販売価格はお問い合わせください</span></p>
+                    <div class="rheaven_cut_wrap">
+                      <div class="rheaven_cut_num clearfix">
+                        <div class="rheaven_cut_minus">-</div>
+                        <div class="rheaven_cut_setnum"><span class="setnum" id="js-setnum">1</span></div>
+                        <div class="rheaven_cut_plus">+</div>
+                      </div>
+                      <div class="rheaven_cut_btn op"><a href="#">カートに入れる</a></div>
+                    </div>
+                    <p class="rheaven_det_txt">コーヒー・ラテ・モカ・ココア・抹茶ラテまで、多彩なメニューが抽出できる、ハイパフォーマンスエスプレッソマシンです。</p>
+                    <p class="rheaven_det_arrow">商品詳細はブランドサイトでご紹介しています</p>
+                    <p class="rheaven_det_bnr op"><a href="#"><img src="./images/rheavendors/bnr_rheaven_det.png" alt=""></a></p>
+                  </div>
+                </div>
+                <!-- /rheaven_det_box -->
+                <div class="rheaven_spec">
+                  <h4 class="page_title_mid"><span class="page_title_en fonten">Spec</span>製品スペック</h4>
+                  <table class="common_table">
+                    <tr>
+                      <th>電源・消費電力・電流</th>
+                      <td>単相200V・2700W・13.5A</td>
+                    </tr>
+                    <tr>
+                      <th>サイズ（幅×奥行×高さ）</th>
+                      <td>422×599×666mm（最大高さ 880mm）</td>
+                    </tr>
+                    <tr>
+                      <th>重量（乾燥重量）</th>
+                      <td>34kg</td>
+                    </tr>
+                    <tr>
+                      <th>コーヒーグラインダー(容量）</th>
+                      <td>2基（500g×2）</td>
+                    </tr>
+                    <tr>
+                      <th>パウダーキャニスター（容量）</th>
+                      <td>3基（980g×3）</td>
+                    </tr>
+                    <tr>
+                      <th>推奨抽出能力</th>
+                      <td>約150杯（/日）</td>
+                    </tr>
+                    <tr>
+                      <th>操作パネル</th>
+                      <td>液晶タッチ式</td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+              <!-- /rheaven_group -->
+            </div>
+            <!-- /contents_main_box -->
+          </div>
+        </div>
+        <!-- /contents_main -->
+      </div>
+      <!-- /contents -->
+
+    </article>
+
+    <?php include './include/footer.php'; ?>
+
+  </div>
+  <!-- /wrapper -->
+<script>
+  var $setnum = $('#js-setnum')
+  $(document).ready(function(){
+    $('.rheaven_cut_minus').click(function(){
+      if(getnum() <= 1){
+        $setnum.text('1')
+      }else{
+        $setnum.text(getnum() - 1)
+      }
+    })
+    $('.rheaven_cut_plus').click(function(){
+      $setnum.text(getnum() + 1)
+    })
+  })
+  function getnum(){
+    return Number($setnum.text())
+  }
+</script>
+</body>
+</html>
