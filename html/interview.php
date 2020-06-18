4c2f84a210e44d05a194d720e886a3b2a157bfee
tuosi <ts-web@bpodts.com.cn>
2020-06-18 10:57:27 +0800

    061801

---
+++ html/interview.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="./css/common.css" media="all" />
+<link rel="stylesheet" type="text/css" href="./css/animate.css"/>
+
+<link rel="stylesheet" type="text/css" href="./css/interview.css"/>
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
+
+<script type="text/javascript" src="./js/jquery.min.js"></script>
+<script type="text/javascript" src="./js/common.js"></script>
+<script type="text/javascript" src="./js/wow.min.js"></script>
+<script type="text/javascript" src="./js/jquery.matchHeight-min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+  <div id="wrapper">
+    <input type="hidden" id="pageID" value="gNav03">
+    <?php include './include/head.php'; ?>
+
+    <article>
+      <div id="contents">
+        <p id="breadcrumb" class="op"><a href="./" class="home fonten">Top</a> > 採用情報</p>
+        <div class="contents_main js-showhead">
+          <div class="inner">
+            <h2 class="page_title"><span class="page_title_en fonten">Recruit</span>採用情報</h2>
+             <ul class="common_bnr common_bnr_two">
+               <li><a href="./recruit.php">採用案内</a></li>
+               <li><a href="./interview.php" class="active">社員インタビュー</a></li>
+             </ul>
+            <!-- /common_bnr -->
+            <div class="inter_photo">
+              <h3 class="page_title_mid">先輩写真インタビュー</h3>
+              <div class="inter_photo_in">
+                <div class="inter_photo_box inter_photo_reverse">
+                  <div class="inter_photo_cont">
+                    <p class="inter_photo_tit">ミスのない丁寧な作業で、<br>全国のマシンの安全稼動を支える</p>
+                    <p>学生時代は茶道部に所属していました。好きなことに関われる仕事がしたいと考え、就職活動ではお茶やコーヒーに関わる企業を受けました。そのなかでブルーマチックジャパンに惹かれたのは、会社説明会で感じた温かい雰囲気と、コーヒーマシンの奥深さに魅せられたからです。<br>現在は、全国の店舗に設置されたコーヒーマシンの点検・修理を管理する部署に所属しています。私の仕事は、受注伝票の処理やメンテナンスのスケジュール調整など。コーヒーマシンの総数は約15,000 台あり、メンテナンスも月間1,500 件ほど行われるため、毎日数多くの書類や問い合わせが私たちのもとに寄せられます。ミスなく対応するのは大変な部分もありますが、全国の店舗のコーヒーの味を支えていると思うと、細かな作業にもやりがいを感じます。<br>今後は「頼れる先輩」を目指して、経験を積んでいきたいです。私自身、周囲の先輩たちの丁寧な指導のおかげで成長できたので、今度は私が後輩の成長をサポートしていけたらと思います。</p>
+                  </div>
+                  <div class="inter_photo_img">
+                    <p><img src="./images/interview/img_interview01.jpg" alt="東日本メンテナンスサービス 管理チーム　堀内梨沙 2018年入社"></p>
+                    <p class="inter_photo_cap">東日本メンテナンスサービス 管理チーム　堀内梨沙<br>2018年入社</p>
+                  </div>
+                </div>
+                <p class="inter_photo_det"><img src="./images/interview/txt_interview01.png" alt=""></p>
+              </div>
+              <!-- /inter_photo_in -->
+              <div class="inter_photo_in">
+                <div class="inter_photo_box">
+                  <div class="inter_photo_cont">
+                    <p class="inter_photo_tit">技術を磨き、<br>多くの人から頼られる存在を目指したい</p>
+                    <p>物心ついた頃から機械が好きで、壊れたゲームのコントローラーを自分で修理するような子どもでした。説明会でのアットホームな雰囲気に惹かれ、ブルーマチックジャパンに入社。現在は、コーヒーマシンの設置作業と保守点検を担当しています。現場は、主にファストフード店とカフェ。1日に3～4店舗回ることが多いです。<br>ブルーマチックジャパンは、とにかく面倒見のいい会社。はじめて1人で現場に出た際、「困ったことがあったらすぐ連絡して」と、先輩が気にかけてくれたことが印象に残っています。私も、同じように後輩に接したいですね。そうすることが、先輩たちへの恩返しになると考えています。<br>先日、地方に出張し、協力会社への技術講習を行いました。入社以来、私が担当している機種の導入に伴っての講習であり、経験を活かしながら、技術を正確に伝えられたと手応えを感じています。何より、上司が私を信頼し、責任ある仕事を任せてくれたことが嬉しく、大きな励みになりました。ゆくゆくは、ブルーマチックジャパンで取り扱っているすべての機種のエキスパートになりたいです。技術を磨き、多くの人に頼られる存在へと成長していきたいと思います。</p>
+                  </div>
+                  <div class="inter_photo_img">
+                    <p><img src="./images/interview/img_interview02.jpg" alt="東日本メンテナンスサービス 技術チーム　田中隆介 2017年入社"></p>
+                    <p class="inter_photo_cap">東日本メンテナンスサービス 技術チーム　田中隆介<br>2017年入社</p>
+                  </div>
+                </div>
+                <p class="inter_photo_det"><img src="./images/interview/txt_interview02.png" alt=""></p>
+              </div>
+              <!-- /inter_photo_in -->
+              <div class="inter_photo_in">
+                <div class="inter_photo_box inter_photo_reverse">
+                  <div class="inter_photo_cont">
+                    <p class="inter_photo_tit">厳しい仕事だからこそ、<br>提案が採用されたときの喜びは格別</p>
+                    <p>もともと食品業界に興味があった私。アルバイト先でブルーマチックジャパンのコーヒーマシンを使用していたことも、入社を決めるきっかけになりました。入社以来、営業として、大手カフェチェーンのお客様に新しいコーヒーマシンを提案しています。また、新規顧客開拓も重要なミッションです。<br>大手チェーン店の強みは、日本全国のどの店舗でも、均一な品質のコーヒーが飲めること。1000店舗を超える規模のお客様との商談には、特に慎重さが求められます。厳しい仕事だからこそ、提案が採用されたときの喜びは格別。お客様と共にコーヒーの品質を守っていくことが私のやりがいです。ブルーマチックジャパンは、若手にチャンスを与えてくれる会社。入社1年目から、大手チェーン店の新規獲得コンペを任されたときは驚きました。経営陣との距離も近く、これから挑戦したいことを直接伝えられます。今、力を入れている機種はセミオートエスプレッソマシン。メーカーからの情報発信をまとめ、社内ワークショップも開催しています。今後も、社内への情報発信を積極的に行い、周囲と力を合わせ、担当ブランドの売上を伸ばしていきたいですね。</p>
+                  </div>
+                  <div class="inter_photo_img">
+                    <p><img src="./images/interview/img_interview03.jpg" alt="営業職 フードサービス営業　鶴田千晶 2012年入社"></p>
+                    <p class="inter_photo_cap">営業職 フードサービス営業　鶴田千晶<br>2012年入社</p>
+                  </div>
+                </div>
+                <p class="inter_photo_det"><img src="./images/interview/txt_interview03.png" alt=""></p>
+              </div>
+              <!-- /inter_photo_in -->
+              <div class="inter_photo_in">
+                <div class="inter_photo_box">
+                  <div class="inter_photo_cont">
+                    <p class="inter_photo_tit">キャリアに関係なく<br>チャンスを与えてくれることが当社の魅力</p>
+                    <p>学生時代は工業系の学科に所属していました。製造業への就職も考えましたが、店舗を回ってコーヒーマシンのメンテナンスを行い、お客様に直接「ありがとう」と言っていただけるブルーマチックジャパンの技術職に惹かれ、入社を決意。昨年から、コンビニエンスストアを担当する技術チームのマネージャーになりました。<br>現在は部下のマネジメントのほか、保守点検の内容の見直しや、協力会社向け技術講習の立案も行っています。特に力を入れているのは、保守点検の機会を利用し、コーヒーマシンの修繕件数を減らしていく取り組み。それに伴い、技術講習の内容も変えていく必要があると考えています。<br>入社年次に関係なく、アイデア次第で型にはまらない仕事ができるのがブルーマチックジャパンの魅力。入社2 年目で、当時スタートしたばかりのコンビニエンスストアのプロジェクトを任され、以来、節目節目で新たな挑戦ができていることがモチベーションになっています。今は、部下の成長が一番のやりがい。数年以内に自分のチームを、会社全体に大きな影響を与えられるチームにしていきたいですね。信頼できる仲間たちと共に、新たな挑戦を続けていきます。</p>
+                  </div>
+                  <div class="inter_photo_img">
+                    <p><img src="./images/interview/img_interview04.jpg" alt="CVS メンテナンスサービス 技術チーム マネージャー　關屋幸斗 2010年入社"></p>
+                    <p class="inter_photo_cap">CVS メンテナンスサービス 技術チーム マネージャー　關屋幸斗<br>2010年入社</p>
+                  </div>
+                </div>
+                <p class="inter_photo_det"><img src="./images/interview/txt_interview04.png" alt=""></p>
+              </div>
+              <!-- /inter_photo_in -->
+            </div>
+            <!-- /inter_photo -->
+            
+            <div class="inter_heart">
+              <h3 class="page_title_mid">先輩社員本音アンケート</h3>
+              <p>お客様のために、社会のために。日々仕事に打ち込むブルーマチックジャパンの社員たち。社会人の先輩である彼らが、どこに惹かれて入社したのか、どんなやりがいを感じているのか。さらにオフのすごし方まで、社員のホンネを集めてみました。</p>
+              <ul class="inter_heart_list">
+                <li>
+                  <p class="inter_heart_img"><img src="./images/interview/map_interview01.png" alt="入社の決め手は？"></p>
+                  <div class="inter_heart_cont hlg01">
+                    <p class="inter_heart_tit">入社の決め手は？</p>
+                    <p>仕事も、社風も、そして立地という声も。さまざまな点に惹かれて入社を決めた方が多いようです。</p>
+                  </div>
+                </li>
+                <li>
+                  <p class="inter_heart_img"><img src="./images/interview/map_interview02.png" alt="どんなときにやりがいを感じる？"></p>
+                  <div class="inter_heart_cont hlg01">
+                    <p class="inter_heart_tit">どんなときにやりがいを感じる？</p>
+                    <p>チームワークや専門知識が求められる仕事だからこそ、やり遂げたときに一番達成感を感じるのかもしれません。</p>
+                  </div>
+                </li>
+                <li>
+                  <p class="inter_heart_img"><img src="./images/interview/map_interview03.png" alt="会社の魅力は？"></p>
+                  <div class="inter_heart_cont hlg01">
+                    <p class="inter_heart_tit">会社の魅力は？</p>
+                    <p>「働きやすい」「成長できる」が多数。「おいしいコーヒーが飲める」の声は、やはり当社ならでは。</p>
+                  </div>
+                </li>
+                <li>
+                  <p class="inter_heart_img"><img src="./images/interview/map_interview04.png" alt="社風を一言でいうと？"></p>
+                  <div class="inter_heart_cont hlg01">
+                    <p class="inter_heart_tit">社風を一言でいうと？</p>
+                    <p>「自由」が圧倒的首位をおさめました。裁量ある環境で、のびのびと働けるのではないでしょうか？</p>
+                  </div>
+                </li>
+                <li>
+                  <p class="inter_heart_img"><img src="./images/interview/map_interview05.png" alt="休日の過ごし方は？"></p>
+                  <div class="inter_heart_cont hlg01">
+                    <p class="inter_heart_tit">休日の過ごし方は？</p>
+                    <p>ゆっくり休む派と、アクティブに動く派、あなたはどちらでしょう？<br>両方を求める方にぴったりの会社かもしれません。</p>
+                  </div>
+                </li>
+                <li>
+                  <p class="inter_heart_img"><img src="./images/interview/map_interview06.png" alt="通勤時間は？"></p>
+                  <div class="inter_heart_cont hlg01">
+                    <p class="inter_heart_tit">通勤時間は？</p>
+                    <p>8 割の方が1 時間以内との結果。地元で長く働きたいという方が多いことがわかりますね。</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <!-- /inter_heart -->
+            
+          </div>
+        </div>
+        <!-- /contents_main -->
+      </div>
+      <!-- /contents -->
+
+    </article>
+
+    <?php include './include/footer.php'; ?>
+
+  </div>
+  <!-- /wrapper -->
+</body>
+</html>
---
+++ html/recruit.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+<link rel="stylesheet" type="text/css" href="./css/common.css" media="all" />
+<link rel="stylesheet" type="text/css" href="./css/animate.css"/>
+
+<link rel="stylesheet" type="text/css" href="./css/recruit.css"/>
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Harmattan&display=swap" rel="stylesheet">
+
+<script type="text/javascript" src="./js/jquery.min.js"></script>
+<script type="text/javascript" src="./js/common.js"></script>
+<script type="text/javascript" src="./js/wow.min.js"></script>
+<script type="text/javascript" src="./js/jquery.matchHeight-min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+  <div id="wrapper">
+    <input type="hidden" id="pageID" value="gNav03">
+    <?php include './include/head.php'; ?>
+
+    <article>
+      <div id="contents">
+        <p id="breadcrumb" class="op"><a href="./" class="home fonten">Top</a> > 採用情報</p>
+        <div class="contents_main js-showhead">
+          <div class="inner">
+            <h2 class="page_title"><span class="page_title_en fonten">Recruit</span>採用情報</h2>
+             <ul class="common_bnr common_bnr_two">
+               <li><a href="./recruit.php" class="active">採用案内</a></li>
+               <li><a href="./interview.php">社員インタビュー</a></li>
+             </ul>
+            <!-- /common_bnr -->
+            <p class="recruit_entry op"><span class="recruit_entry_down">エントリーはこちら</span><br><a href="#"><img src="images/recruit/logo_recruit.jpg" alt=""></a></p>
+            
+            <div class="recruit_section">
+              <h3 class="page_title_mid">コーヒーマシンのある社会をつくる「社会創造業」</h3>
+              <p>私たちブルーマチックジャパンは、業務用コーヒーマシンの専門商社です。<br>アメリカやイタリア、スイスといったコーヒー先進国の世界的メーカーから、高品質なコーヒーマシンを輸入しています。私たちが手掛けたコーヒーマシンが設置されているのは、皆さんも1度は足を運んだことのあるカフェチェーンやファストフードチェーン、レストランチェーン、コンビニエンスストアなど。皆さんの「気軽に美味しいコーヒーが飲める暮らし」のすぐそばに、ブルーマチックジャパンがあります。</p>
+            </div>
+            <!-- /recruit_section -->
+            
+            <div class="recruit_section">
+              <h3 class="page_title_mid">ブルーマチックジャパンのコーヒーマシンは、街中の様々な場所で活躍しています。</h3>
+              <p><img src="./images/recruit/map_recruit.jpg" alt="ブルーマチックジャパンのコーヒーマシンは、街中の様々な場所で活躍しています。"></p>
+            </div>
+            <!-- /recruit_section -->
+            
+            <div class="recruit_section">
+              <h3 class="page_title_mid">主な製品</h3>
+              <ul class="column_four recruit_column">
+                <li>
+                  <div class="column_box">
+                    <p class="column_box_img"><span><img src="./images/recruit/img_recruit01.jpg" alt="JURA"></span></p>
+                    <p class="column_box_det fonten hlg02">JURA</p>
+                    <p class="column_box_txt hlg03">完璧な品質、洗練を極めたスタイル、世界が感嘆したコーヒーの深い味わい。<br>スイスが誇る、コーヒーマシンのワールドブランド「JURA（ユラ）」</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="column_box">
+                    <p class="column_box_img"><span><img src="./images/recruit/img_recruit02.jpg" alt="BREWMATIC"></span></p>
+                    <p class="column_box_det fonten hlg02">BREWMATIC</p>
+                    <p class="column_box_txt hlg03">私たちのライフスタイルに欠かせないものとして根づいたドリップコーヒー文化。<br>「BREWMATIC（ブルーマチック）」は、長年の実績と信頼を誇るコーヒーマシンブランドです。</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="column_box">
+                    <p class="column_box_img"><span><img src="./images/recruit/img_recruit03.jpg" alt="CARIMALI"></span></p>
+                    <p class="column_box_det fonten hlg02">CARIMALI</p>
+                    <p class="column_box_txt hlg03">100年以上の歴史を誇るイタリアのコーヒーマシンブランド「CARIMALI（カリマリ）」。<br>飲みたいコーヒーを培ってきた技術を駆使した最高品質のマシンで提供します。</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="column_box">
+                    <p class="column_box_img"><span><img src="./images/recruit/img_recruit04.jpg" alt="SANREMO"></span></p>
+                    <p class="column_box_det fonten hlg02">SANREMO</p>
+                    <p class="column_box_txt hlg03">イタリアの伝統と、プロフェッショナル達の情熱、世界最高峰の技術がボーダレスに融合。<br>革新的なハイエンドモデルを創造する「SANREMO（サンレモ）。」</p>
+                  </div>
+                </li>
+              </ul>
+              <!-- /column_four -->
+            </div>
+            <!-- /recruit_section -->
+            
+            <div class="recruit_section">
+              <h3 class="page_title_mid">福利厚生</h3>
+              <p>ブルーマチックジャパンでは、働きやすさを重視した様々な福利厚生で、社員が毎日いきいきと仕事に取り組める環境づくりに注力しています。</p>
+              <ul class="recruit_benefits">
+                <li>
+                  <div class="recruit_benefits_box">
+                    <p class="recruit_benefits_ico"><img src="./images/recruit/ico_recruit01.png" alt="よこはまグッドバランス賞"></p>
+                    <p class="recruit_benefits_tit hlg01">よこはまグッドバランス賞</p>
+                    <p class="recruit_benefits_txt hlg02">女性の活躍やワーク・ライフ・バランスを推進し、働きやすい職場環境づくりを積極的に進めている企業として認定されました。</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="recruit_benefits_box">
+                    <p class="recruit_benefits_ico"><img src="./images/recruit/ico_recruit02.png" alt="各種手当"></p>
+                    <p class="recruit_benefits_tit hlg01">各種手当</p>
+                    <p class="recruit_benefits_txt hlg02">・時間外手当　・出張手当<br>・通勤定期代　・役職手当</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="recruit_benefits_box">
+                    <p class="recruit_benefits_ico"><img src="./images/recruit/ico_recruit03.png" alt="ライフサポート"></p>
+                    <p class="recruit_benefits_tit hlg01">ライフサポート</p>
+                    <p class="recruit_benefits_txt hlg02">・退職金（3年以上）　・時短勤務<br>・財形貯蓄<br>・ベネフィットステーション会員<br>・社会保険完備</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="recruit_benefits_box">
+                    <p class="recruit_benefits_ico"><img src="./images/recruit/ico_recruit04.png" alt="休暇"></p>
+                    <p class="recruit_benefits_tit hlg01">休暇</p>
+                    <p class="recruit_benefits_txt hlg02">・有給休暇　　　・産前産後休暇<br>・夏季休暇　　　・慶弔休暇<br>・年末年始休暇　・誕生日休暇</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="recruit_benefits_box">
+                    <p class="recruit_benefits_ico"><img src="./images/recruit/ico_recruit05.png" alt="レクリエーション"></p>
+                    <p class="recruit_benefits_tit hlg01">レクリエーション</p>
+                    <p class="recruit_benefits_txt hlg02">・クラブ活動支援 　・社内懇親会<br>・懇親会補助 </p>
+                  </div>
+                </li>
+                <li>
+                  <div class="recruit_benefits_box">
+                    <p class="recruit_benefits_ico"><img src="./images/recruit/ico_recruit06.png" alt="資格取得支援"></p>
+                    <p class="recruit_benefits_tit hlg01">資格取得支援</p>
+                    <div class="recruit_benefits_txt hlg02">
+                      <p>・コーヒーインストラクター<br>・英会話 など </p>
+                      <p class="recruit_benefits_note">※随時必要な講習やセミナー受講などバックアップ体制あり</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <!-- /recruit_section -->
+            
+          </div>
+        </div>
+        <!-- /contents_main -->
+      </div>
+      <!-- /contents -->
+
+    </article>
+
+    <?php include './include/footer.php'; ?>
+
+  </div>
+  <!-- /wrapper -->
+</body>
+</html>
